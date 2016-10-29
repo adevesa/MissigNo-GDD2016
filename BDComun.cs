544cccd7e20124fe2a2ef60949abc7c8ce42c8fc
Jaugustopose <jaugusto.pose@gmail.com>
2016-10-29 13:13:56 -0300

    Agrego la clase BDComun y app.config para que lo usemos todos

---
+++ BDComun.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ClinicaFrba.Properties;
+using System.Configuration;
+
+namespace ClinicaFrba
+{
+    public class BDComun
+    {
+        public static string ObtenerString()
+        {
+            return Settings.Default.GD2C2016ConnectionString;
+
+        }
+
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection Conn = new SqlConnection(ObtenerString());
+
+        Conn.Open();
+
+            return Conn;
+        }
+
+
+    }
+}
